6b988bb375166441f03e21f96bcef8a78d4bed78
davronmamaraimov1992@gmail.com <davronmamaraimov1996@gmail.com>
2023-04-28 10:41:23 +0500

    Vazifa-4 done

---
+++ vazifa-4.js
@@ -0,0 +1,9 @@
+let number = 23;
+
+let num1 = Math.floor(23 / 10);
+let num2 = (23 % 10);
+
+// console.log(num1);
+// console.log(num2);
+
+console.log("Berilgan son " + number + " edi.Va " + num2 + num1 + " ga o`zgardi!")
